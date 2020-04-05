aca813ce258df8e259cb5e143bc1cf1df08c23b7
Delaram <delaram@Delarams-MacBook-Air.local>
2020-04-06 00:12:15 +0430

    the app is improved

--- src/components/UI/Modal/modal.js
+++ src/components/UI/Modal/modal.js
@@ -3,19 +3,31 @@ import './modal.css'
 import Aux from '../../../hoc/Aux'
 import Backdorp from '../Backdrop/backdrop'
 
-const modal = (props) => (
-  <Aux>
-    <Backdorp show={props.show} clicked={props.modalClosing} />
-    <div
-      className='Modal'
-      style={{
-        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-        opacity: props.show ? '1' : '0'
-      }}
-    >
-      {props.children}
-    </div>
-  </Aux>
-)
+class Modal extends React.Component {
+  shouldComponentUpdate (nextProps, nextState) {
+    // this function should return just boolean
+    return nextProps.show !== this.props.show
+  }
 
-export default modal
+  componentWillUpdate () {
+    console.log('WillUpdate:::')
+  }
+
+  render () {
+    return (
+      <Aux>
+        <Backdorp show={this.props.show} clicked={this.props.modalClosing} />
+        <div
+          className='Modal'
+          style={{
+            transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+            opacity: this.props.show ? '1' : '0'
+          }}
+        >
+          {this.props.children}
+        </div>
+      </Aux>
+    )
+  }
+}
+export default Modal
--- src/components/burger/burgerSummary/burgerSummary.js
+++ src/components/burger/burgerSummary/burgerSummary.js
@@ -3,26 +3,27 @@ import Aux from '../../../hoc/Aux'
 import Button from '../../UI/button/button'
 import '../../UI/button/button.css'
 
-const orderSummary = (props) => {
-  const ingredientsSummary = Object.keys(props.ingredients).map(igkey => {
+class OrderSummary extends React.Component {
+  render () {
+    const ingredientsSummary = Object.keys(this.props.ingredients).map(igkey => {
+      return (
+        <li key={igkey}>
+          <span>{igkey}</span> : {this.props.ingredients[igkey]}
+        </li>
+      )
+    })
     return (
-      <li key={igkey}>
-        <span>{igkey}</span> : {props.ingredients[igkey]}
-      </li>
+      <Aux>
+        <h3>Your Order</h3>
+        <ul>
+          {ingredientsSummary}
+        </ul>
+        <p><strong>Total price : {this.props.price.toFixed(2)} $</strong></p>
+        <p>continue to checkout?</p>
+        <Button className='Danger'>CANCEL</Button>
+        <Button className='Success'>CONTINUE</Button>
+      </Aux>
     )
-  })
-  return (
-    <Aux>
-      <h3>Your Order</h3>
-      <ul>
-        {ingredientsSummary}
-      </ul>
-      <p><strong>Total price : {props.price.toFixed(2)} $</strong></p>
-      <p>continue to checkout?</p>
-      <Button className='Danger'>CANCEL</Button>
-      <Button className='Success'>CONTINUE</Button>
-    </Aux>
-  )
+  }
 }
-
-export default orderSummary
+export default OrderSummary
