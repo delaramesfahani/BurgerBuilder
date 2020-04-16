6094a1a91b3279944e90ea3f1c0fdb91c52cf13c
Delaram <delaram@Delarams-MacBook-Air.local>
2020-04-17 01:10:37 +0430

    Handeling Error Done!

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -144,5 +144,5 @@ class BurgerBuilder extends Component {
     )
   }
 }
-export default withErrorHandler(BurgerBuilder)
+export default withErrorHandler(BurgerBuilder, axios)
  
--- src/hoc/withErrorHandle/withErrorHandler.js
+++ src/hoc/withErrorHandle/withErrorHandler.js
@@ -1,16 +1,42 @@
 import React from 'react'
 import Aux from '../Aux'
 import Modal from '../../components/UI/Modal/modal'
+import { render } from 'react-dom'
 
-cost withErrorHandler = (WrappedComponent) => {
-    return(props) => {
-        return (
-            <Aux>
-                <Modal>something is wrong</Modal>
-                <WrappedComponent {...props}/>
-            </Aux>
-        )
+const withErrorHandler = (WrappedComponent, axios) => {
+  return class extends React.Component {
+    state = {
+        error : null
     }
+    componentDidMount() {
+        axios.interceptors.request.use(
+            null, req => {
+                this.setState({error: null})
+                return req
+            } )
+
+        axios.interceptors.response.use(res => res, error => {
+            this.setState({error: error})
+        } )
+    }
+
+    clickConfirmedHandler = () => {
+        this.setState( {error: null} )
+    }
+
+    render () {
+      return (
+        <Aux>
+          <Modal 
+            show={this.state.error}
+            clicked={this.clickConfirmedHandler}>
+            {this.state.error ? this.state.error.message : null}
+          </Modal>
+          <WrappedComponent {...this.props} />
+        </Aux>
+      )
+    }
+  }
 }
 
 export default withErrorHandler
