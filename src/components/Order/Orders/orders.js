c8184ec2ed19c610951e8044c5ae32dfbb2baf72
Delaram <delaram@Delarams-MacBook-Air.local>
2020-04-28 07:40:27 +0430

    Outputing the orders

--- src/components/Order/Orders/orders.js
+++ src/components/Order/Orders/orders.js
@@ -1,11 +1,28 @@
 import React from 'react'
 import './orders.css'
 
-const order = (props) => (
-  <div className='ordercontainer'>
-    <p>ingredients: Salad (1)</p>
-    <p>Price : <strong>5.6 USD</strong> </p>
-  </div>
-)
+const order = (props) => {
+  const ingredients = []
+  for (const ingKey in props.ingredients) {
+    ingredients.push({
+      name: ingKey,
+      amount: props.ingredients[ingKey]
+    })
+  }
+  console.log('haaaaa', ingredients)
+  const ingOutput = ingredients.map(ig => {
+    return (
+      <div>
+        <span>{ig.name}: {ig.amount}</span>
+      </div>
+    )
+  })
 
+  return (
+    <div className='ordercontainer'>
+      <p>{props.customer}</p>
+      <p><strong>ingredients:</strong> {ingOutput}</p>
+    </div>
+  )
+}
 export default order
--- src/containers/Orders/orders.js
+++ src/containers/Orders/orders.js
@@ -1,15 +1,38 @@
 import React from 'react'
 import Order from '../../components/Order/Orders/orders'
-import { withRouter } from 'react-router-dom'
+import axios from '../../axios-orders'
+import withErrorHandler from '../../hoc/withErrorHandle/withErrorHandler'
 
 class Orders extends React.Component {
+  state = {
+    loading: true,
+    orders: []
+  }
+
+  componentDidMount() {
+    axios.get('/orders.json')
+    .then( res => {
+      const fetchData = []
+      for( let key in res.data ) {
+        fetchData.push({ ...res.data[key], id: key })
+      }
+      this.setState({ loading: false, orders: fetchData })
+      console.log('orders::',fetchData)
+    })
+    .catch( err => this.setState({ loading: false }))
+  }
+
   render () {
     return (
       <div>
-        <Order />
-        <Order />
+        { this.state.orders.map(order =>
+           (<Order 
+             key={ order.id }
+             ingredients={ order.ingredients }
+             customer={ order.customer.name}
+           />)) }
       </div>
     )
   }
 }
-export default Orders
+export default withErrorHandler(Orders, axios)
